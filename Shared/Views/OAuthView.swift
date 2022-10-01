b47c673d155adacd75e40df76c7980ccf340715c
Kristian Pennacchia <clubsport7@hotmail.com>
2022-10-01 14:50:42 +1000

    Abstracted loading animation into HeartbeatActivityIndicator.

--- Shared/Views/OAuthView.swift
+++ Shared/Views/OAuthView.swift
@@ -21,9 +21,6 @@ struct OAuthView: View {
 
     @State private var oAuth: YoutubeOAuth?
     @State private var showAuthError = false
-    @State private var heartbeatChanged = false
-
-    private let heartbeatTimer = Timer.publish(every: 0.6, on: .main, in: .common).autoconnect()
 
     var body: some View {
         ZStack {
@@ -64,21 +61,7 @@ struct OAuthView: View {
                     Spacer()
                 }
             } else {
-                ZStack {
-                    Circle()
-                        .frame(width: 200, height: 200)
-                        .foregroundColor(heartbeatChanged ? .white.opacity(0.8) : .brand.youtube)
-                        .animation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3))
-                    Image(systemName: "heart.fill")
-                        .foregroundColor(heartbeatChanged ? .brand.youtube : .white)
-                        .font(.system(size: 100))
-                        .scaleEffect(heartbeatChanged ? 1.0 : 0.5)
-                        .animation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3))
-                }
-                .animation(.default)
-                .onReceive(heartbeatTimer) { _ in
-                    heartbeatChanged.toggle()
-                }
+                HeartbeatActivityIndicator()
             }
         }
         .onAppear {
