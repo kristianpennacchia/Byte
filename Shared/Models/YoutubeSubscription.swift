794e6c81af627cef96fd82b47ad59ec5d945be82
Kristian Pennacchia <clubsport7@hotmail.com>
2022-10-01 16:31:51 +1000

    Created YoutubeSubscription model.

---
+++ Shared/Models/YoutubeSubscription.swift
@@ -0,0 +1,47 @@
+//
+//  YoutubeSubscription.swift
+//  Byte
+//
+//  Created by Kristian Pennacchia on 1/10/2022.
+//  Copyright © 2022 Kristian Pennacchia. All rights reserved.
+//
+
+import Foundation
+
+struct YoutubeSubscription: Decodable {
+    struct Snippet: Decodable {
+        struct ResourceID: Decodable {
+            let kind: String
+            let channelId: String
+        }
+
+        struct Thumbnail: Decodable {
+            struct URL: Decodable {
+                let url: String
+            }
+
+            let `default`: URL
+            let medium: URL
+            let high: URL
+        }
+
+        let publishedAt: String
+        let title: String
+        let description: String
+        let resourceId: ResourceID
+        let channelId: String
+        let thumbnails: [Thumbnail]
+    }
+
+    struct ContentDetail: Decodable {
+        let totalItemCount: Int
+        let newItemCount: Int
+        let activityType: String
+    }
+
+    let kind: String
+    let etag: String
+    let id: String
+    let snippet: Snippet
+    let contentDetails: ContentDetail
+}
