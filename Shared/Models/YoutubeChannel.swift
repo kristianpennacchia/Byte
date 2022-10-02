881cb706cb800b6211a1f3eb3bb50bf7daa55656
Kristian Pennacchia <clubsport7@hotmail.com>
2022-10-02 22:11:44 +1100

    Created some Youtube models.

---
+++ Shared/Models/YoutubeChannel.swift
@@ -0,0 +1,55 @@
+//
+//  YoutubeChannel.swift
+//  Byte
+//
+//  Created by Kristian Pennacchia on 2/10/2022.
+//  Copyright © 2022 Kristian Pennacchia. All rights reserved.
+//
+
+import Foundation
+
+/// https://developers.google.com/youtube/v3/docs/channels#resource
+struct YoutubeChannel: Decodable {
+    struct Snippet: Decodable {
+        struct Thumbnails: Decodable {
+            struct URL: Decodable {
+                let url: String
+            }
+
+            let `default`: URL
+            let medium: URL
+            let high: URL
+        }
+
+        let title: String
+        let description: String
+        let customUrl: String?
+        let publishedAt: String
+        let thumbnails: Thumbnails
+    }
+
+    struct ContentDetails: Decodable {
+        struct RelatedPlaylists: Decodable {
+            let likes: String
+            let uploads: String
+        }
+
+        let relatedPlaylists: RelatedPlaylists
+    }
+
+    struct Statistics: Decodable {
+        let viewCount: String
+        let subscriberCount: String
+        let hiddenSubscriberCount: Bool
+        let videoCount: String
+    }
+
+    static let part = "snippet,contentDetails,statistics"
+
+    let kind: String
+    let etag: String
+    let id: String
+    let snippet: Snippet
+    let contentDetails: ContentDetails
+    let statistics: Statistics
+}
