1beada0ab5719d8e1e7c225e5d484ddac5f89458
Kristian Pennacchia <clubsport7@hotmail.com>
2022-10-07 17:19:26 +1100

    Created Channelable protocol.

---
+++ Shared/Models/Channelable.swift
@@ -0,0 +1,42 @@
+//
+//  Channelable.swift
+//  Byte
+//
+//  Created by Kristian Pennacchia on 7/10/2022.
+//  Copyright © 2022 Kristian Pennacchia. All rights reserved.
+//
+
+import Foundation
+
+func equalsChannelable(lhs: any Channelable, rhs: any Channelable) -> Bool {
+    return lhs.id == rhs.id
+}
+
+func compareChannelable(lhs: any Channelable, rhs: any Channelable) -> Bool {
+    return lhs.displayName < rhs.displayName
+}
+
+let channelPreview = Channel(
+    id: App.previewUsername,
+    login: App.previewUsername,
+    displayName: App.previewUsername,
+    profileImageUrl: ""
+)
+
+protocol Channelable: Identifiable, Equatable, Hashable, Comparable {
+    var id: String { get }
+    var displayName: String { get }
+    var profileImageUrl: String { get }
+}
+
+extension Channelable where Self: Equatable {
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        equalsChannelable(lhs: lhs, rhs: rhs)
+    }
+}
+
+extension Channelable where Self: Comparable {
+    static func < (lhs: Self, rhs: Self) -> Bool {
+        compareChannelable(lhs: lhs, rhs: rhs)
+    }
+}
